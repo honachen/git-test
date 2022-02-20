f010d1a249e9e1d318cafc265dfbbd52003fecaf
v_honachen <v_honachen@foxmail.com>
2022-02-20 22:28:34 +0800

    Hot-fix 1commit~

--- src/main/java/com/honachen/git/gitText.java
+++ src/main/java/com/honachen/git/gitText.java
@@ -5,5 +5,6 @@ public class gitText {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("hello gitlab");
     }
 }
